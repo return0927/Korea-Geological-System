068bdbe79c16e886990f3c2be3ffaf6af2bbd22c
R3turn0927 <bjleh0927@gmail.com>
2018-01-29 02:33:03 +0900

    DB Structure

---
+++ structure.sql
@@ -0,0 +1,60 @@
+CREATE TABLE busan (
+    uid character varying(17),
+    name character varying(100),
+    subname character varying(100),
+    upground_floors integer,
+    underground_floors integer,
+    height double precision,
+    built_date character varying(8),
+    updated_date character varying(8),
+    zip integer,
+    shp character varying(30)
+);
+CREATE TABLE daegu (
+    uid character varying(17),
+    name character varying(100),
+    subname character varying(100),
+    upground_floors integer,
+    underground_floors integer,
+    height double precision,
+    built_date character varying(8),
+    updated_date character varying(8),
+    zip integer,
+    shp character varying(30)
+);
+CREATE TABLE daejeon (
+    uid character varying(17),
+    name character varying(100),
+    subname character varying(100),
+    upground_floors integer,
+    underground_floors integer,
+    height double precision,
+    built_date character varying(8),
+    updated_date character varying(8),
+    zip integer,
+    shp character varying(30)
+);
+CREATE TABLE incheon (
+    uid character varying(17),
+    name character varying(100),
+    subname character varying(100),
+    upground_floors integer,
+    underground_floors integer,
+    height double precision,
+    built_date character varying(8),
+    updated_date character varying(8),
+    zip integer,
+    shp character varying(30)
+);
+CREATE TABLE seoul (
+    uid character varying(17),
+    name character varying(100),
+    subname character varying(100),
+    upground_floors integer,
+    underground_floors integer,
+    height double precision,
+    built_date character varying(8),
+    updated_date character varying(8),
+    zip integer,
+    shp character varying(30)
+);
