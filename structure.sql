4bb4a3ef3b680aba0ba6f9c915c240ad025ef09c
R3turn0927 <bjleh0927@gmail.com>
2018-01-29 02:37:29 +0900

    Simplified DB Structure generator

--- structure.sql
+++ structure.sql
@@ -11,50 +11,14 @@ CREATE TABLE busan (
     shp character varying(30)
 );
 CREATE TABLE daegu (
-    uid character varying(17),
-    name character varying(100),
-    subname character varying(100),
-    upground_floors integer,
-    underground_floors integer,
-    height double precision,
-    built_date character varying(8),
-    updated_date character varying(8),
-    zip integer,
-    shp character varying(30)
+  LIKE busan
 );
 CREATE TABLE daejeon (
-    uid character varying(17),
-    name character varying(100),
-    subname character varying(100),
-    upground_floors integer,
-    underground_floors integer,
-    height double precision,
-    built_date character varying(8),
-    updated_date character varying(8),
-    zip integer,
-    shp character varying(30)
+LIKE busan
 );
 CREATE TABLE incheon (
-    uid character varying(17),
-    name character varying(100),
-    subname character varying(100),
-    upground_floors integer,
-    underground_floors integer,
-    height double precision,
-    built_date character varying(8),
-    updated_date character varying(8),
-    zip integer,
-    shp character varying(30)
+LIKE busan
 );
 CREATE TABLE seoul (
-    uid character varying(17),
-    name character varying(100),
-    subname character varying(100),
-    upground_floors integer,
-    underground_floors integer,
-    height double precision,
-    built_date character varying(8),
-    updated_date character varying(8),
-    zip integer,
-    shp character varying(30)
+LIKE busan
 );
